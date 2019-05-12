c63128ebfebbe080e6f58bf81ee57329f89cd345
sccdyuyu <sccdyuyu@gmail.com>
2019-05-12 22:53:43 +0800

    9章作业

---
+++ 9.10/Time.h
@@ -0,0 +1,24 @@
+#ifndef TIME_H_INCLUDED
+#define TIME_H_INCLUDED
+class Time
+{
+public:
+   Time( int = 0, int = 0, int = 0 );
+   bool setTime( int, int, int );
+   bool setHour( int );
+   bool setMinute( int );
+   bool setSecond( int );
+   int getHour();
+   int getMinute();
+   int getSecond();
+   void printUniversal();
+   void printStandard(); e
+private:
+   int hour;
+   int minute;
+   int second;
+};
+
+
+
+#endif // TIME_H_INCLUDED
